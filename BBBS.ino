3f5a45a80c1a0c2120c87e1b0f1543c64b5ad566
Tyler Nappy <tylernappy@gmail.com>
2015-02-23 15:54:49 -0500

    switched min and max value for sensor

--- BBBS.ino
+++ BBBS.ino
@@ -4,8 +4,8 @@ int potPin = A5;
 int brightness = 0;
 
 int val = 0;
-int sensorMin = 1023;
-int sensorMax = 0;
+int sensorMin = 0;
+int sensorMax = 1023;
 
 void setup() {
   // put your setup code here, to run once:
@@ -15,8 +15,7 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   val = analogRead(potPin);
-  brightness = map(val, sensorMin, sensorMax, 0, 255); 
+  brightness = map(val, sensorMin, sensorMax, 0, 255);
   analogWrite(ledPin, brightness);
   delay(30);
 }
-
