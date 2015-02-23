5f6595dd0838545713458c44ddd3ef835950809c
Tyler Nappy <tylernappy@gmail.com>
2015-02-23 15:49:19 -0500

    buzzer and pot

--- BBBS.ino
+++ BBBS.ino
@@ -1,22 +1,22 @@
-int ledPin = 9;
+int buzzerPin = 9;
 
 int potPin = A5;
-int brightness = 0;
+int loudness = 0;
 
 int val = 0;
-int sensorMin = 1023;
-int sensorMax = 0;
+int sensorMin = 0;
+int sensorMax = 1023;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(ledPin, OUTPUT);
+  pinMode(buzzerPin, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   val = analogRead(potPin);
-  brightness = map(val, sensorMin, sensorMax, 0, 255); 
-  analogWrite(ledPin, brightness);
+  loudness = map(val, sensorMin, sensorMax, 0, 255); 
+  analogWrite(buzzerPin, val);
   delay(30);
 }
 
